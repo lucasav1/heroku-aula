a4c014375c144230c281fb47343489f2ef20c7c7
Bruno Pimentel <brunoapimentel@gmail.com>
2019-09-30 09:25:28 -0300

    Liga debug

--- coach/settings.py
+++ coach/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=z&zgcqm=*hx_k-kguwpx3l4l7d@y-0zzv18a)vxrk3c*n0y4w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
